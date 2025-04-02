3c2d99b2d89898ac75b062ac7d3a36970f40bf7d
“Lee <lee.hudson@diamond.ac.uk>
2025-04-02 14:16:28 +0100

    More

--- convert.py
+++ convert.py
@@ -63,7 +63,13 @@ def remove_unwanted_tags(input_xml):
                      "Hy8401ip.auto_Hy8401ip",
                      "dlsPLC.read100",
                      "FastVacuum.Master16",
-                     "FastVacuum.auto_Channel16"]
+                     "FastVacuum.auto_Channel16",
+                     "FastVacuum.auto_ChannelUn",
+                     "TimingTemplates.defaultEVR",
+                     "SR-VA.auto_psu24vStatus",
+                     "rackFan.rackFan",
+                     "IOCinfo.IOCinfo",
+                     "FINS.FINSTemplate"]
     
     # Remove elements with unwanted tags
     for elem in root.findall("*"):
@@ -74,7 +80,24 @@ def remove_unwanted_tags(input_xml):
     output = '<?xml version="1.0" ?>\n' + ET.tostring(root, encoding='unicode')
     return output
 
-def add_fins_udp_init(input_xml):
+# Returns index of last instance on element found
+def find_last_element_index(root,target):
+
+    last_target = None
+    for elem in root.findall(target):
+        last_target = elem
+    
+    # If we found an asyn.AsynIP element, insert after it
+    if last_target is not None:
+        # Find the position to insert after the last asyn.AsynIP
+        for i, elem in enumerate(root):
+            if elem == last_target:
+                return i
+        return -1
+    else:
+        return -1
+
+def add_fins_udp_init(input_xml,after=None):
     tree = ET.ElementTree(ET.fromstring(input_xml))
     root = tree.getroot()
     
@@ -83,29 +106,24 @@ def add_fins_udp_init(input_xml):
     fins_elem.set("ip", "192.168.5.10")
     fins_elem.set("name", "VLVCC_01_FINS")
     fins_elem.set("simulation", "None")
-    
-    # Find the last asyn.AsynIP element
-    last_asyn_ip = None
-    for elem in root.findall("asyn.AsynIP"):
-        last_asyn_ip = elem
-    
-    # If we found an asyn.AsynIP element, insert after it
-    if last_asyn_ip is not None:
-        # Find the position to insert after the last asyn.AsynIP
-        for i, elem in enumerate(root):
-            if elem == last_asyn_ip:
-                root.insert(i + 1, fins_elem)
-                break
+
+
+    if after is not None:
+        position = find_last_element_index(root,after)
+        if position > 0:
+            root.insert(position + 1, fins_elem)
+        else:
+            root.append(fins_elem)
     else:
-        # If no asyn.AsynIP is found, append at the end of the root
         root.append(fins_elem)
-    
+
+
     # Output modified XML with declaration
     output = '<?xml version="1.0" ?>\n' + ET.tostring(root, encoding='unicode')
     output = output.replace('><',">\n\t<")
     return output
 
-def add_NX102_readReal(input_xml):
+def add_NX102_readReal(input_xml, after = None):
     tree = ET.ElementTree(ET.fromstring(input_xml))
     root = tree.getroot()
     elementList = list()
@@ -116,23 +134,36 @@ def add_NX102_readReal(input_xml):
         id = elem.get("id")
         name = elem.get("name")
         dom = elem.get("dom")
-        vc = 1
-        c = ""
-        if gaugeChannels[name] == "A":
-            c="1"
+        vc = gaugeConfig[name]["vlvcc"]
+        slot = gaugeConfig[name]["slot"]
+        chan = ""
+        if slot == "A":
+            chan="1"
         else:
-            c="2"
+            chan="2"
 
         readReadElem = ET.Element("dlsPLC.NX102_readReal")
         readReadElem.set("device", f"{dom}-VA-GAUGE-{id}:RAW")
         readReadElem.set("name", f"PLC_COMB0{index}")
         readReadElem.set("port", "VLVCC_01_EIP")
-        readReadElem.set("tag", f"{dom}_VC{vc}_GCTLR_{id}_COMB_{gaugeChannels[name]}_C{c}_Comb_mBar")
+        readReadElem.set("tag", f"{dom}_VC{vc}_GCTLR_{id}_COMB_{slot}_C{chan}_Comb_mBar")
         elementList.append(readReadElem)
 
+    if after is not None:
+        position = find_last_element_index(root,after)
+    else:
+        position = -1
+
     for elem in elementList:
-        root.append(elem)
-    
+        if position < 0:
+            root.append(elem)
+        else:
+            root.insert(position + 1, elem)
+            position += 1
+
+        
+
+
     output = '<?xml version="1.0" ?>\n' + ET.tostring(root, encoding='unicode')
     output = output.replace('><',">\n\t<")
     return output
@@ -142,21 +173,22 @@ def add_NX102_readReal(input_xml):
 input_filename = "SR06C-VA-IOC-01.xml"
 output_filename = input_filename.replace(".xml", "_converted.xml")
 cell = 6
-gaugeChannels = {"GAUGE_S_01":"A",
-                 "GAUGE_S_02":"B",
-                 "GAUGE_A_01":"A",
-                 "GAUGE_A_02":"B",
-                 "GAUGE_A_03":"B",
-                 "GAUGE_A_31":"A",
-                 "GAUGE_A_04":"B"}
+gaugeConfig = {"GAUGE_S_01":{"slot":"A","vlvcc":"1"},
+               "GAUGE_S_02":{"slot":"B","vlvcc":"1"},
+               "GAUGE_A_01":{"slot":"A","vlvcc":"1"},
+               "GAUGE_A_02":{"slot":"B","vlvcc":"1"},
+               "GAUGE_A_03":{"slot":"B","vlvcc":"2"},
+               "GAUGE_A_31":{"slot":"A","vlvcc":"2"},
+               "GAUGE_A_04":{"slot":"B","vlvcc":"2"}}
+
 
 with open(input_filename, "r") as file:
     input_xml = file.read()
 
 converted_xml = remove_unwanted_tags(input_xml)
 converted_xml = substitueAsynPorts(converted_xml)
-converted_xml = add_fins_udp_init(converted_xml)
-converted_xml = add_NX102_readReal(converted_xml)
+converted_xml = add_fins_udp_init(converted_xml,"asyn.AsynIP")
+converted_xml = add_NX102_readReal(converted_xml,after="FINS.FINSUDPInit")
 
 
 
